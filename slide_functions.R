8daf72b1c7888de78c6d51616de3e25bba79a577
muschellij2 <muschellij2@gmail.com>
2017-09-19 14:34:45 -0400

    updated

--- slide_functions.R
+++ slide_functions.R
@@ -10,13 +10,23 @@ bg_slide = function(
   suffix = ".png",
   titles = rep("", length(files_in_order)),
   positions = rep("center", length(files_in_order)),
-  size = rep("100%", length(files_in_order))
+  size = rep("100%", length(files_in_order)),
+  add_opts = ""
 ){
   n_files = length(files_in_order)
   files_in_order = paste0(files_in_order, suffix)
   folder = rep(folder, length.out = n_files)
   size = rep_len(size, length.out = n_files)
-  top_cat = paste0("---\nbackground-image: url(", folder)
+  top_cat = "---"
+  add_opts = add_opts[ add_opts != "" ]
+  if (length(add_opts) > 0) {
+    add_opts = paste(add_opts, collapse = "\n")
+    add_opts = paste0(add_opts, "\n")
+  } else {
+    add_opts = ""
+  }
+  top_cat = paste(top_cat, add_opts, sep = "\n")
+  top_cat = paste0(top_cat,  "background-image: url(", folder)
   bottom_cat = paste0(")\nbackground-size: ", size, " \n")
   bottom_cat = paste0(bottom_cat, paste0(
     "background-position: ", positions, "\n")
