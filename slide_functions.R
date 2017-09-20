a23beaf7dce3916b94d8267cfedb5cc38a183517
muschellij2 <muschellij2@gmail.com>
2017-09-20 16:50:20 -0400

    updated to 08 slides

--- slide_functions.R
+++ slide_functions.R
@@ -11,12 +11,14 @@ bg_slide = function(
   titles = rep("", length(files_in_order)),
   positions = rep("center", length(files_in_order)),
   size = rep("100%", length(files_in_order)),
+  footnotes = "",
   add_opts = ""
 ){
   n_files = length(files_in_order)
   files_in_order = paste0(files_in_order, suffix)
   folder = rep(folder, length.out = n_files)
   size = rep_len(size, length.out = n_files)
+  footnotes = rep_len(footnotes, length.out = n_files)
   top_cat = "---"
   add_opts = add_opts[ add_opts != "" ]
   if (length(add_opts) > 0) {
@@ -25,6 +27,7 @@ bg_slide = function(
   } else {
     add_opts = ""
   }
+  add_opts = rep_len(add_opts, length.out = n_files)
   top_cat = paste(top_cat, add_opts, sep = "\n")
   top_cat = paste0(top_cat,  "background-image: url(", folder)
   bottom_cat = paste0(")\nbackground-size: ", size, " \n")
@@ -34,6 +37,9 @@ bg_slide = function(
   keep = !titles %in% ""
   titles[  keep ] = paste0("\n# ", titles[keep])
   bottom_cat = paste0(bottom_cat, titles)
+  if (any(footnotes != "")) {
+    bottom_cat = paste0(bottom_cat, "\n.footnote[", footnotes, "]")  
+  }
   res = paste0(top_cat, files_in_order, bottom_cat, "\n")
   cat(res, sep = "")
 }
